e7e909cc45507ffe58fe866a95b9d1d7694165d7
Jose Bustos <35240264+korbek99@users.noreply.github.com>
2024-11-19 19:44:47 -0300

    Initial Commit

---
+++ UserItemsExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UserItemsExam
+//
+//  Created by Jose Preatorian on 19-11-24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
